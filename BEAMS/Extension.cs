3b5472d1576974fb683225638b3c01cf0a39d9ea
Daichi Tanaka <daichi.tanaka@proaxia-consulting.co.jp>
2023-07-14 10:11:51 +0900

    Implement Extension.cs

---
+++ BEAMS/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEAMS
+{ 
+    public static class Extension
+    {
+        public static string Deliminate(this int value)
+        {
+            return String.Format("{0:#,0}", value);
+        }
+    }
+}
---
+++ BEAMSTests/ExtensionTests.cs
@@ -0,0 +1,25 @@
+﻿using BEAMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEAMSTests
+{
+    [TestClass]
+    public class ExtensionTests
+    {
+        [TestMethod]
+        public void ConfirmDeliminate()
+        {
+            var expected1 = "999";
+            var result1 = Extension.Deliminate(999);
+            Assert.AreEqual(expected1, result1);
+
+            var expected2 = "1,000";
+            var result2 = Extension.Deliminate(1000);
+            Assert.AreEqual(expected2, result2);
+        }
+    }
+}
